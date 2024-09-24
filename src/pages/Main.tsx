2aad78100ccd7a15f0f6401e3e4aa12610da0b52
sominparrrk <sompark98@gmail.com>
2024-09-25 04:56:41 +0900

    feat: implement in main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,16 @@
+import Button from '../components/Button/Button';
+import Input from '../components/Input/Input';
+
 const Main = () => {
-  return <div className='text-3xl font-bold underline'>EV Charging Simulator</div>;
+  return (
+    <div>
+      <div className='main__header w-screen p-4 bg-black'>
+        <h1 className='text-3xl font-bold text-green-300'>EV Charging Simulator</h1>
+      </div>
+      <Button label='button' onClick={() => console.log('click')} />
+      <Input label='input' onChange={() => console.log('change')} />
+    </div>
+  );
 };
 
 export default Main;
