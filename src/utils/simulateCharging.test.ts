a492c11cc60723046b808ded83cba83f17ae1bfb
sominparrrk <sompark98@gmail.com>
2024-09-26 16:06:45 +0900

    feat: implement simulator logic

---
+++ src/utils/simulateCharging.test.ts
@@ -0,0 +1,50 @@
+import { chargingDemandProbabilitiesData } from '../lib/chargingDemandProbabilitiesData';
+import {
+  DEFAULT_CHARGE_POWER,
+  DEFAULT_CHARGING_NEEDS,
+  DEFAULT_MULTIPLIER,
+  DEFAULT_NUM_CHARGEPOINTS,
+  getArrivalProbability,
+  simulateCharging,
+} from './simulateCharging';
+
+describe('simulateCharging function', () => {
+  it('should return expected result for 20 chargepoints', () => {
+    const result = simulateCharging(
+      DEFAULT_NUM_CHARGEPOINTS,
+      DEFAULT_MULTIPLIER,
+      DEFAULT_CHARGE_POWER,
+      DEFAULT_CHARGING_NEEDS
+    );
+
+    expect(result.theoreticalMaxPowerDemand).toBe(
+      DEFAULT_NUM_CHARGEPOINTS * DEFAULT_CHARGE_POWER
+    );
+    expect(result.actualMaxPowerDemand).toBeLessThanOrEqual(
+      result.theoreticalMaxPowerDemand
+    );
+    expect(result.concurrencyFactor).toBeGreaterThanOrEqual(35);
+    expect(result.concurrencyFactor).toBeLessThanOrEqual(55);
+    expect(result.actualMaxPowerDemand).toBeGreaterThanOrEqual(77);
+    expect(result.actualMaxPowerDemand).toBeLessThanOrEqual(121);
+  });
+
+  it('should calculate arrival probability correctly', () => {
+    const mockHour = 0;
+    const probabilityWithoutMultiply = getArrivalProbability(mockHour, 0);
+    const probabilityWithMultiply = getArrivalProbability(mockHour, 100);
+
+    expect(probabilityWithoutMultiply).toBe(0.0094);
+    expect(probabilityWithMultiply).toBe(0.0188);
+  });
+
+  it('should calculate charging demand probability correctly', () => {
+    const randomSpy = jest.spyOn(Math, 'random').mockReturnValue(0.3);
+    const chargingDemand = chargingDemandProbabilitiesData.find(
+      (d) => d.km === 100
+    );
+
+    expect(chargingDemand?.km).toBe(100);
+    randomSpy.mockRestore();
+  });
+});
